47b54f90f95514244156baf233883a047fb2cbac
Dinesh <2431032@cognizant.com>
2025-11-30 19:03:33 +0530

    Fix TypeScript errors for deployment1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import CelestialBackground from './components/CelestialBackground'
 import HeaderResponsive from './components/HeaderResponsive'
 import ScrollProgress from './components/ScrollProgress'
